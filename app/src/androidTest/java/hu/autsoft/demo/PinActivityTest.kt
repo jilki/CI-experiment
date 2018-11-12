e699e244a27bd4352c3ab8b8fdd9b12f0ef0e80b
Thamó Zsolt <thamo.zsolt@autsoft.hu>
2018-11-12 14:00:35 +0100

    proper R imports

--- app/src/androidTest/java/hu/autsoft/demo/PinActivityTest.kt
+++ app/src/androidTest/java/hu/autsoft/demo/PinActivityTest.kt
@@ -8,6 +8,7 @@ import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import hu.autsoft.demo.ui.pin.PinActivity
+import hu.autsoft.hwsw.demo.pin.zsolt.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
